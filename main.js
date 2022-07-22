788d95641a8dce1cccd0e8f61cfdc78394a0485d
Tony1074 <109608354+Tony1074@users.noreply.github.com>
2022-07-22 16:41:21 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,62 @@
+
+var random = prompt("я загадал число от 1 до 10 угадай это число у тебя 5 попыток");
+
+if (random < 5) {
+    alert("мало")
+}
+else if (random > 5) {
+    alert("много")
+}
+else {
+   var win = alert("верно")
+}
+
+
+var random = prompt("я загадал число от 1 до 10 угадай это число 4 попыток");
+
+if (random < 5) {
+    alert("мало")
+}
+else if (random > 5) {
+    alert("много")
+}
+else {
+    alert("верно")
+}
+
+var random = prompt("я загадал число от 1 до 10 угадай это число 3 попыток");
+
+if (random < 5) {
+    alert("мало")
+}
+else if (random > 5) {
+    alert("много")
+}
+else {
+    alert("верно")
+}
+
+var random = prompt("я загадал число от 1 до 10 угадай это число 2 попыток");
+
+if (random < 5) {
+    alert("мало")
+}
+else if (random > 5) {
+    alert("много")
+}
+else {
+    alert("верно")
+}
+
+var random = prompt("я загадал число от 1 до 10 угадай это число 1 попыток");
+
+if (random < 5) {
+    alert("мало")
+}
+else if (random > 5) {
+    alert("много")
+}
+else {
+    alert("верно")
+}
+
